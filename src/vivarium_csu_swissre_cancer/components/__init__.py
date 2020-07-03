3963ae921939c12d909d6919bd06a614bb94bc0b
Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>
2020-07-02 19:07:24 -0700

    add screening observer

--- src/vivarium_csu_swissre_cancer/components/__init__.py
+++ src/vivarium_csu_swissre_cancer/components/__init__.py
@@ -1,3 +1,4 @@
 from vivarium_csu_swissre_cancer.components.disease import BreastCancer
-from vivarium_csu_swissre_cancer.components.observers import DisabilityObserver, DiseaseObserver, MortalityObserver
+from vivarium_csu_swissre_cancer.components.observers import (DisabilityObserver, DiseaseObserver, MortalityObserver,
+                                                              ScreeningObserver)
 from vivarium_csu_swissre_cancer.components.screening import ScreeningAlgorithm
