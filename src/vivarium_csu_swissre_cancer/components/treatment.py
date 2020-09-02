70e454b4e1d41ed545f952790cdcc9e9d067fd7b
Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>
2020-09-02 16:21:50 -0700

    finalize parameters and add treatment coverage

--- src/vivarium_csu_swissre_cancer/components/treatment.py
+++ src/vivarium_csu_swissre_cancer/components/treatment.py
@@ -1,16 +1,21 @@
 """Treatment model."""
+import numpy as np
+import pandas as pd
 import typing
 
 from vivarium_csu_swissre_cancer import globals as project_globals
+from vivarium_csu_swissre_cancer.utilities import get_triangular_dist_random_variable
 
 
 if typing.TYPE_CHECKING:
     from vivarium.framework.engine import Builder
+    from vivarium.framework.population import SimulantData
 
 
 # Columns
 AGE = 'age'
 SEX = 'sex'
+TREATMENT_PROPENSITY = 'treatment_propensity'
 
 
 class TreatmentEffect:
@@ -24,15 +29,28 @@ class TreatmentEffect:
     # noinspection PyAttributeOutsideInit
     def setup(self, builder: 'Builder'):
         self.clock = builder.time.clock()
-        self.randomness = builder.randomness.get_stream(self.name)
-
         draw = builder.configuration.input_data.input_draw_number
+
         self.efficacy = {
-            project_globals.POSITIVE_LCIS_STATE_NAME: project_globals.LCIS_TREATMENT_EFFICACY.get_random_variable(draw),
-            project_globals.POSITIVE_DCIS_STATE_NAME: project_globals.DCIS_TREATMENT_EFFICACY.get_random_variable(draw),
+            project_globals.POSITIVE_LCIS_STATE_NAME: (
+                np.exp(project_globals.TREATMENT.LOG_LCIS_EFFICACY.get_random_variable(draw))
+            ),
+            project_globals.POSITIVE_DCIS_STATE_NAME: project_globals.TREATMENT.DCIS_EFFICACY.get_random_variable(draw),
+        }
+
+        self.coverage = {
+            project_globals.POSITIVE_LCIS_STATE_NAME: get_triangular_dist_random_variable(
+                *project_globals.TREATMENT.LCIS_COVERAGE_PARAMS, 'lcis_treatment_coverage', draw
+            ),
+            project_globals.POSITIVE_DCIS_STATE_NAME: get_triangular_dist_random_variable(
+                *project_globals.TREATMENT.DCIS_COVERAGE_PARAMS, 'dcis_treatment_coverage', draw
+            )
         }
 
         required_columns = [project_globals.SCREENING_RESULT_MODEL_NAME]
+        columns_created = [TREATMENT_PROPENSITY]
+        builder.population.initializes_simulants(self.on_initialize_simulants, creates_columns=columns_created)
+
         self.population_view = builder.population.get_view(required_columns)
 
         builder.value.register_value_modifier(
@@ -40,7 +58,7 @@ class TreatmentEffect:
             modifier=lambda index, target: self.treat(index, target, project_globals.POSITIVE_LCIS_STATE_NAME),
             # TODO add intermediate pipeline between screening result and treatment to allow for less than full
             #  treatment coverage
-            requires_columns=[project_globals.SCREENING_RESULT_MODEL_NAME]
+            requires_columns=[project_globals.SCREENING_RESULT_MODEL_NAME, TREATMENT_PROPENSITY]
         )
 
         builder.value.register_value_modifier(
@@ -48,11 +66,16 @@ class TreatmentEffect:
             modifier=lambda index, target: self.treat(index, target, project_globals.POSITIVE_DCIS_STATE_NAME),
             # TODO add intermediate pipeline between screening result and treatment to allow for less than full
             #  treatment coverage
-            requires_columns=[project_globals.SCREENING_RESULT_MODEL_NAME]
+            requires_columns=[project_globals.SCREENING_RESULT_MODEL_NAME, TREATMENT_PROPENSITY]
         )
+
+    def on_initialize_simulants(self, pop_data: 'SimulantData'):
+        propensity = pd.Series(np.random.uniform(size=len(pop_data.index)), index=pop_data.index)
+        self.population_view.update(propensity)
 
     def treat(self, index, target, state_name):
         pop = self.population_view.get(index)
-        screening_result = pop.loc[:, project_globals.SCREENING_RESULT_MODEL_NAME]
-        lcis_positive_mask = screening_result == state_name
-        return target * (1 - self.efficacy[state_name] * lcis_positive_mask)
+        would_be_treated_if_positive = pop.loc[:, TREATMENT_PROPENSITY] < self.coverage[state_name]
+        is_positive = pop.loc[:, project_globals.SCREENING_RESULT_MODEL_NAME] == state_name
+        is_treated = would_be_treated_if_positive & is_positive
+        return target * (1 - self.efficacy[state_name] * is_treated)
